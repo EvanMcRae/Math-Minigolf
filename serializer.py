a0a9463e3310182a54ab3b79eac1edbce58fdb21
Evtema3 <evanmmcrae@gmail.com>
2022-12-14 23:51:57 -0500

    Update serializer.py

--- serializer.py
+++ serializer.py
@@ -14,8 +14,6 @@ for i in range (0, len(levelData["levels"])):
     thisLevel = level.Level.from_json(levelData["levels"][i])
     Levels.append(thisLevel)
 
-# print(type(Levels[0].goal))
-
 for i in range(0, len(Levels)):
     print(Levels[i])
     print("\n")
