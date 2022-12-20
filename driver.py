ce0740f9227c6e0559e1c58ad8bb5149422adef5
mschuck1 <suprm2001@gmail.com>
2022-12-20 17:18:33 -0500

    very descriptive

--- driver.py
+++ driver.py
@@ -1,1 +1,76 @@
-print('testing commit')
+# Random ideas/notes
+# Cool to have multiple sprites for the flag so it can wave a bit (animation)
+
+import pygame
+
+
+# Glabal variables here
+levels = []
+
+
+# Utility functions go here
+
+def loadLevels(filename):
+    print()
+
+def parseInput(input):
+    print()
+
+def getUserInput():
+    print()
+
+
+
+# Starting pygame stuff
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+fieldImg = pygame.image.load("field.png")
+fieldCheckeredImg = pygame.image.load("field_checkered.png")
+holeImg = pygame.image.load("hole.png")
+flagImg = pygame.image.load("flag.png")
+flagNoHoleImg = pygame.image.load("flag_no_hole.png")
+ballImg = pygame.image.load("ball.png")
+holeImg = pygame.image.load("hole.png")
+
+# Pygame related functions here
+
+# We might want to make this apart of the ball class
+def animateBallMovement(destination):
+    #would probably be good to solve an ODE to calcuclate ball path that looks good (realistic friction) and ends up at the right location.
+    #If that's too difficult, something that looks DECENT is: 
+    # k = 2.9
+    # x coord = 1/(t^k)
+    # t = [2, 10]. t is the time step of the animation.
+    # We could scale this as needed, using
+    print()
+
+def drawField():
+
+
+
+
+
+
+# Main loop here
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
